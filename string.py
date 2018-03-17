be51446bb045123536b794e3f3b668be2d16490a
arihu <arihu>
2018-03-17 12:47:04 +0800

    python data type

---
+++ string.py
@@ -0,0 +1,15 @@
+hello = 'hello'
+world = 'world'
+print(hello)
+print(hello + world)
+print(len(hello + world))
+hw = hello + ' ' + world
+print(hw)
+print(len(hw))
+hw2 = '%s %s %d' % (hw, hello, 12)
+print(hw2)
+
+s = 'hello xi dada'
+print(s.capitalize())
+print(s.upper())
+
