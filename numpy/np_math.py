e87510c303909c2e86f022dfcf8be1834d169d6e
arihu <arihu>
2018-03-18 14:22:08 +0800

    数据类型相关

--- numpy/np_math.py
+++ numpy/np_math.py
@@ -31,3 +31,29 @@ print(np.sqrt(x))
 # print
 # [[1.         1.41421356]
 #  [1.73205081 2.        ]]
+
+# 求和
+print(np.sum(x))
+# print 10
+print(np.sum(x, axis=0))  # Compute sum of each column
+# print [4,6]
+print(np.sum(x, axis=1))  # Compute sum of each row;
+# print [3,7]
+
+# Broadcasting
+# We will add the vector v to each row of the matrix x,
+# storing the result in the matrix y
+x = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]])
+v = np.array([1, 0, 1])
+y = np.empty_like(x)  # Create an empty matrix with the same shape as x
+
+# Add the vector v to each row of the matrix x with an explicit loop
+for i in range(4):
+    y[i, :] = x[i, :] + v  # 相当于取出X的每一行元素，加V
+
+# Now y is the following
+# [[ 2  2  4]
+#  [ 5  5  7]
+#  [ 8  8 10]
+#  [11 11 13]]
+print(y)
