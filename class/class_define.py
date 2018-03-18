f6517b0b19df61db54d5b57bf93bcfad223d683c
arihu <arihu>
2018-03-18 12:21:09 +0800

    数据类型相关

---
+++ class/class_define.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+class Greetor():
+    # Constructor
+    def __init__(self, name):
+        self.name = name
+
+    def greet(self, loud=False):
+        if (loud):
+            print('Hello,%s' % self.name.upper())
+        else:
+            print('hello,%s' % self.name)
+
+
+g = Greetor('Fredmen');
+g.greet()
+g.greet(True)
