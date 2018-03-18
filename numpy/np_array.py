4aba23b66256f2ca38e16b7b19b3dc79b7003338
arihu <arihu>
2018-03-18 13:25:45 +0800

    数据类型相关

--- numpy/np_array.py
+++ numpy/np_array.py
@@ -16,3 +16,18 @@ print(a)
 
 b[0, 1] = 55
 print(a)  # 注意区分b[0,1]与b[0:1]
+
+# array indexing
+a = np.array([[1, 2], [3, 4], [5, 6]])
+print(a[[0, 1, 2], [0, 1, 0]])  # 对a取第1，2，3行，然后分别取第1，2，1列，print [1 4 5]
+
+# Boolean index
+arr = np.array([[1, 2, 3, 4],
+                [2, 4, 6, 8],
+                [3, 6, 9, 12],
+                [4, 8, 12, 16]])
+mask = arr > 5
+
+print 'boolean mask is:'
+print mask
+print arr[mask]
