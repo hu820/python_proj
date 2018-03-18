883f2cbe87bd26f69f9bbae9012f6badb1909c70
arihu <arihu>
2018-03-18 13:28:25 +0800

    数据类型相关

--- numpy/np_array.py
+++ numpy/np_array.py
@@ -4,6 +4,12 @@
 # Numpy offers several ways to index into arrays.
 # Slicing: Similar to Python lists, numpy arrays can be sliced.
 # Since arrays may be multidimensional, you must specify a slice for each dimension of the array:
+# 切片和索引的同异（：与，）
+# 切片和索引都是访问多维数组中元素的方法，这是两者的共同点，不同之处有:
+#
+# 切片得到的是原多维数组的一个 视图(view) ，修改切片中的内容会导致原多维数组的内容也发生变化
+# 切片得到在多维数组中连续(或按特定步长连续)排列的值，而索引可以得到任意位置的值，自由度更大一些
+
 import numpy as np
 
 a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
