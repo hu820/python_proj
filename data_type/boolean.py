466427b88a746c246de37b5aa57de6bd17be1679
Lighthouse <345150259@qq.com>
2018-03-24 15:49:56 +0800

    Update boolean.py

--- data_type/boolean.py
+++ data_type/boolean.py
@@ -5,6 +5,6 @@ print(t and f)  # Logical AND; prints "False"
 print(t or f)  # Logical OR; prints "True"
 print(not t)  # Logical NOT; prints "False"
 print(t != f)  # Logical XOR; prints "True"
-print(t)
+print(!t)
 print(t and f)
 
