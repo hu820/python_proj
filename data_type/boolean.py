3da508f5d8777b2669350279748218f504380584
arihu <arihu>
2018-03-24 15:42:41 +0800

    test

--- data_type/boolean.py
+++ data_type/boolean.py
@@ -5,5 +5,8 @@ print(t and f)  # Logical AND; prints "False"
 print(t or f)  # Logical OR; prints "True"
 print(not t)  # Logical NOT; prints "False"
 print(t != f)  # Logical XOR; prints "True"
+
 print(t and f)
+
+print(f and t)
 
