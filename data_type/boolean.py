d68dea239cf50ee6325a86d07f66be696d6cac36
arihu <arihu>
2018-03-31 11:00:15 +0800

    fix bug

--- data_type/boolean.py
+++ data_type/boolean.py
@@ -6,14 +6,6 @@ print(t or f)  # Logical OR; prints "True"
 print(not t)  # Logical NOT; prints "False"
 print(t != f)  # Logical XOR; prints "True"
 print(t and f)
-print(t and f)
-print(t and f)
-print(t and f)
-print(t and f)
-print(t and f)
-print(t and f)
-print(t and f)
-print(t and f)
-print(t and f)
+
 
 
